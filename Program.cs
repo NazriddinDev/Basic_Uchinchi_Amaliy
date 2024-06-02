112feabf6e43a1a4b0b28a29f3747734458b54ea
qurbonovnazriddin@gmail.com <qurbonovnazriddin@gmail.com>
2024-06-02 23:42:32 +0500

    INFRA: Tub sonlar

--- Program.cs
+++ Program.cs
@@ -1,1 +1,24 @@
-﻿
+﻿using System;
+// Tub sonlar
+int n;
+Console.Write("Son kiriting=> ");
+n=Convert.ToInt32(Console.ReadLine());
+if (n == 2)
+{
+    Console.WriteLine("Tub son...");
+}
+else if (n == 3)
+{
+    Console.WriteLine("Tub son...");
+}
+else if (n == 5)
+{
+    Console.WriteLine("Tub son...");
+}
+else if(n%2==0||n%3==0||n%4==0||n%5==0)
+{
+    Console.WriteLine("Tub son emas...");
+}
+else Console.WriteLine("Tub son...");
+
+//
