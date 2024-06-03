a9a560ba2cccf4bc58fb996f02d282d2cc01bb71
qurbonovnazriddin@gmail.com <qurbonovnazriddin@gmail.com>
2024-06-04 00:58:53 +0500

    INFRA: Sonning darajasini hisoblash

--- Program.cs
+++ Program.cs
@@ -1,40 +1,40 @@
 ﻿using System;
-// // Tub sonlar
-// int n;
-// Console.Write("Son kiriting=> ");
-// n=Convert.ToInt32(Console.ReadLine());
-// if (n == 2)
-// {
-//     Console.WriteLine("Tub son...");
-// }
-// else if (n == 3)
-// {
-//     Console.WriteLine("Tub son...");
-// }
-// else if (n == 5)
-// {
-//     Console.WriteLine("Tub son...");
-// }
-// else if(n%2==0||n%3==0||n%4==0||n%5==0)
-// {
-//     Console.WriteLine("Tub son emas...");
-// }
-// else Console.WriteLine("Tub son...");
+// Tub sonlar
+int n;
+Console.Write("Son kiriting=> ");
+n=Convert.ToInt32(Console.ReadLine());
+if (n == 2)
+{
+    Console.WriteLine("Tub son...");
+}
+else if (n == 3)
+{
+    Console.WriteLine("Tub son...");
+}
+else if (n == 5)
+{
+    Console.WriteLine("Tub son...");
+}
+else if(n%2==0||n%3==0||n%4==0||n%5==0)
+{
+    Console.WriteLine("Tub son emas...");
+}
+else Console.WriteLine("Tub son...");
 
-// //Bo'luvchisiz sonlar
-// int k;
-// Console.Write("Son kiriting=> ");
-// k=Convert.ToInt32(Console.ReadLine());
-// for(int i=2; i<k; i++)
-// {
-//     if(k%i==0)
-//     {
-//         Console.WriteLine(i);
-//     }
+//Bo'luvchisiz sonlar
+int k;
+Console.Write("Son kiriting=> ");
+k=Convert.ToInt32(Console.ReadLine());
+for(int i=2; i<k; i++)
+{
+    if(k%i==0)
+    {
+        Console.WriteLine(i);
+    }
 
-// }
+}
 
-//Daraja hisoblash
+// Daraja hisoblash
 int a,b;
 Console.Write("Son kiriting=> ");
 a=Convert.ToInt32(Console.ReadLine());
